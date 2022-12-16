32336acb7b892796c543fdce4fdb2cf0701f8b1f
JaRoD41 <laurent.trinco@gmail.com>
2022-12-16 17:09:32 +0100

    test js validation function ok

--- modal.js
+++ modal.js
@@ -60,15 +60,69 @@ formSubmit.addEventListener('submit', function (e) {
 
 //********************* affichage des messages d'erreur  ***********************************/
 
-console.log(firstInput.value)
 // clear validation message
-function clearValidationMessage(element) {
-	element.setAttribute('data-error-visible', 'false')
-	element.setAttribute('data-error', '')
-}
+// function clearValidationMessage(element) {
+// 	element.setAttribute('data-error-visible', 'false')
+// 	element.setAttribute('data-error', '')
+// }
 
 // set validation message
-function setValidationMessage(element, message) {
-	element.setAttribute('data-error-visible', 'true')
-	element.setAttribute('data-error', message)
+// function setValidationMessage(element, message) {
+// 	element.setAttribute('data-error-visible', 'true')
+// 	element.setAttribute('data-error', message)
+// }
+
+const form = document.querySelector('form[name="reserve"]')
+const field = form.querySelectorAll('.text-control')
+
+function validateField(field) {
+	// Récupération de la valeur du champ
+	const value = field.value.trim()
+
+	// Récupération du conteneur parent du champ
+	const container = field.closest('.formData')
+
+	// Get the error message element
+	const errorMessage = container.querySelector('.error-message')
+
+	// Initialisation de la variable de validité à true
+	let isValid = true
+
+	// Vérification de la longueur de la valeur du champ
+	if (value.length < 2) {
+		container.setAttribute(
+			'data-error',
+			'Le champ doit contenir au moins 2 caractères'
+		)
+		container.setAttribute('data-error-visible', true)
+		isValid = false
+	}
+
+	// Vérification du format de l'adresse e-mail
+	if (field.type === 'email' && !value.includes('@')) {
+		container.setAttribute(
+			'data-error',
+			'Le champ doit contenir une adresse e-mail valide'
+		)
+		container.setAttribute('data-error-visible', true)
+		isValid = false
+	}
+
+	// Si le champ est valide, suppression de la classe d'erreur et de la classe de style d'erreur visible
+	if (isValid) {
+		container.removeAttribute('data-error')
+		container.setAttribute('data-error-visible', false)
+	}
+
+	return isValid
 }
+
+form.addEventListener('change', (event) => {
+	event.preventDefault()
+
+	// récupère le champs de formulaire concerné
+	const currentField = event.target
+
+	// valide le champs en cours
+	validateField(currentField)
+})
